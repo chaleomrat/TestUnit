5f439e4953d860b566b1253398f41698d12244c2
chaleomrat <614259007@webmail.npru.ac.th>
2021-03-18 12:33:37 +0700

    first commit

---
+++ Calender.php
@@ -0,0 +1,14 @@
+
+<?php
+$month = $_POST["month"];
+$day = $_POST["day"];
+class Calender
+{
+    public function Showdate($month,  $day)
+    {
+        $data = "2021-" . $month . "-" . $day;
+        $unixTimestamp = strtotime($data);
+        $dayOfWeek = date("l", $unixTimestamp);
+        return $dayOfWeek;
+    }
+}
